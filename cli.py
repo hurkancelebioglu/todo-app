d90b886f712f160845889eab08f888afef8fce71
hurkan <hurkan.celebioglu@gmail.com>
2023-09-18 17:43:05 +0300

    Final touch

--- cli.py
+++ cli.py
@@ -1,66 +1,68 @@
+# from functions import get_todos, write_todos
 import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
-print(f"It is {now}")
+print("It is", now)
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if (user_action.lower()).startswith("add"):
+    if user_action.startswith("add"):
         todo = user_action[4:]
 
         todos = functions.get_todos()
 
-        todos.append(todo + "\n")
+        todos.append(todo + '\n')
 
         functions.write_todos(todos)
 
-    elif (user_action.lower()).startswith("show"):
-        todos = functions.get_todos()
-        for index, item in enumerate(todos, 1):
-            item = (item.strip("\n")).title()
-            print(f"{index}-{item}")
+    elif user_action.startswith('show'):
 
-    elif (user_action.lower()).startswith("edit"):
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    elif user_action.startswith('edit'):
         try:
-            number_edit = int(user_action[5:])
-            number_edit = number_edit - 1
+            number = int(user_action[5:])
+            print(number)
+
+            number = number - 1
 
             todos = functions.get_todos()
 
-            new_todo = input("Enter a new todo: ")
-            todos[number_edit] = new_todo + "\n"
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
 
             functions.write_todos(todos)
         except ValueError:
-            print("Your command is not valid. Please provide an integer.")
+            print("Your command is not valid.")
             continue
 
-    elif (user_action.lower()).startswith("complete"):
-
+    elif user_action.startswith('complete'):
         try:
-            number_complete = int(user_action[9:])
+            number = int(user_action[9:])
 
             todos = functions.get_todos()
-
-            removed_todo = todos[number_complete - 1].strip("\n")
-            todos.pop(number_complete - 1)
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
 
             functions.write_todos(todos)
 
-            message = f"Todo {removed_todo} was removed from the list."
+            message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
         except IndexError:
             print("There is no item with that number.")
             continue
 
-    elif (user_action.lower()).startswith("exit"):
+    elif user_action.startswith('exit'):
         break
-
     else:
         print("Command is not valid.")
-
 
 print("Bye!")
--- gui.py
+++ gui.py
@@ -4,66 +4,65 @@ import time
 
 Sg.theme("Black")
 
-label_time = Sg.Text("", key="label_time")
-label = Sg.Text("Type to-do")
+clock = Sg.Text('', key='clock')
+label = Sg.Text("Type in a to-do")
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
-add_button = Sg.Button("Add")
-list_box = Sg.Listbox(values=functions.get_todos(), key="todos_list",
+add_button = Sg.Button("Add", size=10)
+list_box = Sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = Sg.Button("Edit")
 complete_button = Sg.Button("Complete")
 exit_button = Sg.Button("Exit")
 
-window = Sg.Window("My To-do App",
-                   layout=[[label_time],
+window = Sg.Window('My To-Do App',
+                   layout=[[clock],
                            [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
-                   font=("Helvetica", "20"))
+                   font=('Helvetica', 20))
 
 while True:
     event, values = window.read(timeout=200)
-    now = time.strftime("%b %d, %Y %H:%M:%S")
-    window["label_time"].update(value=now)
-
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] + "\n"
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-            window["todos_list"].update(values=todos)
+            window['todos'].update(values=todos)
 
         case "Edit":
             try:
-                todo_to_edit = values["todos_list"][0]
-                new_todo = values["todo"]
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+
+                if not new_todo.endswith('\n'):
+                    new_todo = new_todo + '\n'
+
                 todos = functions.get_todos()
                 index = todos.index(todo_to_edit)
                 todos[index] = new_todo
                 functions.write_todos(todos)
-                window["todos_list"].update(values=todos)
+                window['todos'].update(values=todos)
             except IndexError:
-                Sg.popup("Please select an item first.", font=("Helvetica", "20"))
+                Sg.popup("Please select an item first.", font=('Helvetica', 20))
 
         case "Complete":
             try:
-                todo_to_complete = values["todos_list"][0]
+                todo_to_complete = values['todos'][0]
                 todos = functions.get_todos()
                 todos.remove(todo_to_complete)
                 functions.write_todos(todos)
-                window["todos_list"].update(values=todos)
-                window["todo"].update(value="")
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
             except IndexError:
-                Sg.popup("Please select an item first.", font=("Helvetica", "20"))
-
-        case "todos_list":
-            window["todo"].update(value=values["todos_list"][0])
-
+                Sg.popup("Please select an item first.", font=("Helvetica", 20))
         case "Exit":
             break
-
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case Sg.WIN_CLOSED:
             break
 
